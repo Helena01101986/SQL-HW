96915a4fd55cc949ee58954ae0fd44f6b9fb293e
Helen Eremina <karpova01101986@gmail.com>
2023-05-19 20:35:57 +0300

    init commit

---
+++ src/test/java/tests/SQLTests.java
@@ -0,0 +1,44 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import data.DataHelper;
+import data.SQLHelper;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import page.LoginPage;
+import static com.codeborne.selenide.Selenide.open;
+import static data.SQLHelper.cleanDatabase;
+
+public class SQLTests {
+
+    @AfterAll
+    static void teardown() {
+        cleanDatabase();
+    }
+
+    @Test
+    @DisplayName("should successful login")
+    void shouldSuccessfulLogin() {
+            open("http://localhost:9999");
+            Configuration.holdBrowserOpen = true;
+            var loginPage = new LoginPage();
+            var authInfo = DataHelper.getAuthInfoForUser();
+            var verificationPage = loginPage.validLogin(authInfo);
+            var verificationCode = SQLHelper.getVerificationCode();
+            verificationPage.validVerify(verificationCode.getCode());
+    }
+
+    @Test
+    @DisplayName("should successful for random user")
+    void shouldSuccessfulForRandomUser() {
+        open("http://localhost:9999");
+        Configuration.holdBrowserOpen = true;
+        var loginPage = new LoginPage();
+        var authInfo = DataHelper.generateRandomUser();
+        loginPage.validLogin(authInfo);
+        loginPage.errorNotification();
+
+    }
+
+}
